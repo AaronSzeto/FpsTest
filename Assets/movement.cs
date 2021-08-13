dd1b380e1196e92ac344f760fb081364c3bf7aba
AaronSzeto <aaron.n.szeto@gmail.com>
2021-08-13 16:47:21 -0700

    test

---
+++ Assets/movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float player_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Vertical") * player_speed;
+        transform.Translate(Vector3.forward * movement, Space.Self);
+    }
+    private void Update()
+    {
+
+    }
+}
